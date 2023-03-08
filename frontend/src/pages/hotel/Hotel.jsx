b540f9e7ad35e64fb870e57d4db4a6b4d77c9033
SreeparnaPaul <sreeparnapaul21@gmail.com>
2023-03-08 21:43:36 +0530

    #changes

--- frontend/src/pages/hotel/Hotel.jsx
+++ frontend/src/pages/hotel/Hotel.jsx
@@ -14,6 +14,7 @@ import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
 import { SearchContext } from "../../context/SearchContext";
+import { useContext } from "react";
 
 const Hotel = () => {
   const location= useLocation()
